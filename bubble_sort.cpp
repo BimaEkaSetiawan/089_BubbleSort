f9bc693dc2af408da49de17b002cc1d29144b199
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-03-13 19:40:28 +0700

    first commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int arr[10]; // variabel public array
+int nData;
+
+void inputData()
+{
+    while (true)
+    {
+        cout << "Masukan Jumlah Data ";
+        cin >> nData;
+
+        if (nData < 10)
+        {
+           break;
+        }
+        else
+        {
+            cout << "Jumlah Data Maksimal 10" << endl;
+        }
+    }
+    
+    cout << "===============" << endl;
+    cout << "Masukan Data" << endl;
+    cout << "===============" << endl;
+    cout << endl;
+
+    for (int i  = 0; i < nData; i++)
+    {
+        cout << "Input Data ke-" << i + 1 << "=";
+        cin >> arr[i];
+    }
+    
+}
+
+void bubbleSort(){
+    int pass = 1; // step 1
+
+   do
+   {
+    for (int j = 0; j <= nData - 1- pass; j++) // step 2
+    {
+        if (arr[j] > arr[j + 1]) // step 3
+        {
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j +1];
+            arr[j +1] = temp;
+        }
+         
+    }
+    pass = pass + 1; // step 4
+
+   } while (pass <= nData - 1); 
+}
+
+void tampilData(){
+    cout << "Tampil Data " << endl;
+    cout << endl;
+
+    for (int i = 0; i < nData; i++)
+    {
+        cout << "Data ke-" << i + 1 << "=" << arr[i] << endl;
+    }
+    
+}
+
+int main()
+{
+    inputData();
+    bubbleSort();
+    tampilData();
+    return 0;
+}
